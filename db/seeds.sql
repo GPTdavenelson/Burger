a0b0b5f883f450d1ee4f7824983a5ed305f546cc
GPTdavenelson <60995712+GPTdavenelson@users.noreply.github.com>
2020-09-17 17:29:25 -0400

    Upload

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers_db (name) VALUES ('Banzai Burger');
+INSERT INTO burgers_db (name) VALUES ('Sautéed Shroom Burger');
+INSERT INTO burgers_db (name, devoured) VALUES ('BurninLove Burger', true);
+INSERT INTO burgers_db (name, devoured) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burgers_db (name, devoured) VALUES ('Black & Blue Burger', true);
+INSERT INTO burgers_db (name) VALUES ('Spicy Burger');
