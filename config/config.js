88196a73cb3ff7d8c40c8faf4786675ed41ac22e
Dave Nelson <gptdavenelson@gmail.com>
2020-09-21 08:10:30 -0700

    updating project with heroku config vars

---
+++ config/config.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const config =  
+{
+    development: {
+        user: "root",
+        password: "root",
+        database: "burgers_db",
+        host: "127.0.0.1",
+        port: 3306,
+        dialect: "mysql"
+    },
+        production: {
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: "heroku_e904f3c24c0202f",
+        host: process.env.CLEARDB_DATABASE_URL,
+        port: 3306,
+        dialect: "mysql"
+    }
+}
+module.exports = config
--- config/connection.js
+++ config/connection.js
@@ -4,9 +4,12 @@ const mysql = require('mysql');
 // Listens to what enviornment is running from start script (package.json)
 // and uses the correct MySql configuration from config.json
 const env = process.env.NODE_ENV || 'development'; // defaults to development if none configured
-const config = require('./config.json')[env];
 
-let connection = mysql.createConnection(config);
+const config = require('./config');
+const envConfig = config[env]
+
+
+let connection = mysql.createConnection(envConfig);
 
 // Make connection.
 connection.connect(function (err) {
